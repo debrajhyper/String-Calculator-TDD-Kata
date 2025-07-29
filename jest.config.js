72ab8bd065e784828f80478273ba0703b6d760d1
Debraj Karmakar <debrajkarmakar010@gmail.com>
2025-07-29 12:15:26 +0530

    chore[project]: initialize Node + TypeScript + Jest boilerplate - Install and configure TypeScript compiler (tsconfig.json) - Integrate Jest via ts‑jest for TypeScript testing

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+import { createDefaultPreset } from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+  ],
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
+
+export default config;
